f56cd5538fbb0fad286185072efc72831c560a51
Aurea <fu.sakura@gmail.com>
2019-01-05 14:54:37 -0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import FrontPage from './components/auth/FrontPage'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+import Dashboard from './components/dashboard/Dashboard'
+import Profile from './components/profile/Profile'
+import CreateGroup from './components/groups/CreateGroup'
+import Navbar from './components/layout/Navbar'
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        Hello, World!
+        <h1>The Lending Library</h1>
+        <FrontPage />
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+          <Route path= '/profile' component={Profile} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/creategroup' component={CreateGroup} />
+        </Switch>
       </div>
+    </BrowserRouter>
     );
   }
 }
---
+++ src/components/auth/FrontPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FrontPage = () => {
+  return (
+    <div>
+      This is the FrontPage.
+    </div>
+  )
+}
+
+export default FrontPage
