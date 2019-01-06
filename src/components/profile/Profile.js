0e81f8c3141c5c28184782dcd1f7ba999eca3f39
Aurea <fu.sakura@gmail.com>
2019-01-06 14:48:09 -0800

    add nav bar and redirect to profile page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import Navbar from '../layout/Navbar'
 
-const Profile = () => {
+const Profile = (props) => {
+  const { auth } = props;
+  if (!auth.uid) return <Redirect to='/frontpage' />
   return (
     <div>
+      <Navbar />
       This is the Profile.
     </div>
   )
 }
 
-export default Profile
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  }
+}
+
+export default connect(mapStateToProps)(Profile)
