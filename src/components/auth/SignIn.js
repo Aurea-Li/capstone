1b48652f71c774738c70da1a1c3ac33288fbe592
Aurea <fu.sakura@gmail.com>
2019-01-06 13:50:35 -0800

    add signin signup functionality

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/authActions'
+import { Redirect } from 'react-router-dom'
 
 class SignIn extends Component {
-  render() {
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.signIn(this.state);
+  }
+
+  render () {
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to='/' />
+
     return (
-      <div>
-        SignIn
-      </div>
-    );
+
+      <form onSubmit={this.onSubmit} >
+        <h2>Sign In</h2>
+
+        <section className="error-messages">
+          { authError ? <p>Error: {authError}</p> : null}
+        </section>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" className="form-control" onChange={this.onChange}/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" className="form-control" onChange={this.onChange}/>
+        </div>
+
+
+        <button className="btn btn-primary">Login</button>
+
+      </form>
+
+    )
   }
 }
 
-export default SignIn;
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError,
+    auth: state.firebase.auth
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,13 +1,78 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signUp } from '../../store/actions/authActions'
+import { Redirect } from 'react-router-dom'
 
 class SignUp extends Component {
+
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.signUp(this.state);
+  }
+
   render() {
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to='/' />
+
     return (
-      <div>
-        SignUp
-      </div>
+
+        <form onSubmit={this.onSubmit} >
+          <h5>Sign Up</h5>
+
+        <section className="error-messages">
+          { authError ? <p>Error: {authError}</p> : null}
+        </section>
+
+          <div className="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" className="form-control" onChange={this.onChange}/>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" className="form-control" onChange={this.onChange}/>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" className="form-control" onChange={this.onChange}/>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" className="form-control" onChange={this.onChange}/>
+          </div>
+          <button className="btn btn-primary">Sign Up</button>
+        </form>
+
     );
   }
 }
 
-export default SignUp;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
