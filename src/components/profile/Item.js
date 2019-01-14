644dfb68811c9fce948b07c154f031d693899b59
Aurea <fu.sakura@gmail.com>
2019-01-14 15:35:34 -0800

    add date to request info in profile

--- src/components/profile/Item.js
+++ src/components/profile/Item.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import moment from 'moment'
 
 const Item = ({ item, removeItem }) => {
 
   const { title, authors, img } = item;
 
+
   const status = item.status ? <p>Status: {item.status}</p> : null;
-  const borrowerInfo = item.borrowerID ? <p>Borrowed by: {item.borrowerFirstName} {item.borrowerLastName}</p> : null;
+
+  let borrowerInfo;
+  if (item.borrowerID){
+
+    borrowerInfo = item.borrowerID ? <p>Borrowed by: {item.borrowerFirstName} {item.borrowerLastName} on {moment(new Date(item.borrowedDate.seconds * 1000)).calendar()}</p> : null;
+  }
+
 
   return (
     <div className="container">
